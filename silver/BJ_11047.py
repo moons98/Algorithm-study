747d38ed73d9ea10cd1eb332e4753044955f8cc0
SANGIN MUN <munsi2003@hanyang.ac.kr>
2023-09-07 16:44:57 +0900

    silver4, greedy

--- silver/BJ_11047.py
+++ silver/BJ_11047.py
@@ -12,9 +12,14 @@ coin.sort(reverse=True)
 answer = 0
 for i in coin:
     while K >= i:
-        K -= i
-        answer += 1
+        tmp = K // i
+        K -= tmp * i
+        answer += tmp
 
     if not K:
         print(answer)
         break
+
+"""
+단순히 반복문으로 빼도 될 것 같았는데, 몫 계산 안하면 시간초과 발생
+"""
